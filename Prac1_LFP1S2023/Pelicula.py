696e5e365d11f4db0f84e361c69f1e7e28f4841b
David-Matias-07 <125230944+David-Matias-07@users.noreply.github.com>
2023-02-24 23:41:03 -0600

    Subiendo Practica

---
+++ Prac1_LFP1S2023/Pelicula.py
@@ -0,0 +1,8 @@
+class Pelicula:
+
+    def __init__(self, nombre, actores, año, genero):
+        self.nombre = nombre
+        self.actores = actores
+        self.año = año
+        self.genero = genero
+
