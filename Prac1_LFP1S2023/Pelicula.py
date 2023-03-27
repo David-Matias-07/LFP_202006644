d621ce1ecea22d69188a869cf8e6046570305d56
David-Matias-07 <125230944+David-Matias-07@users.noreply.github.com>
2023-03-26 23:42:29 -0600

    Delete Prac1_LFP1S2023 directory

--- Prac1_LFP1S2023/Pelicula.py
+++
@@ -1,8 +0,0 @@
-class Pelicula:
-
-    def __init__(self, nombre, actores, año, genero):
-        self.nombre = nombre
-        self.actores = actores
-        self.año = año
-        self.genero = genero
-
